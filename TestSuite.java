da9e97be8ecef83a3ac3d6021d068bcd3f6a2946
Anuneet Anand <anuneet18022@iiitd.ac.in>
2020-05-08 22:42:38 +0530

    Delete TestSuite.java

--- TestSuite.java
+++
@@ -1,7 +0,0 @@
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(value=Suite.class)
-@Suite.SuiteClasses(value={ TestSaveCheckPoint.class,TestCricket.class, TestSnake.class, TestVulture.class, TestTrampoline.class, TestGameWinner.class})
-public class TestSuite { }
